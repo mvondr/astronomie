3c69a59ab33dd9e999f9937b1192de6a26e6a33d
mvondr <mvondr@gmail.com>
2022-08-24 21:45:09 +0200

    Service Sunrise-Sunset added

--- python-docker/app.py
+++ python-docker/app.py
@@ -1,10 +1,18 @@
 import os
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def status():
+    return "{\"service\": \"sunrise-sunset\"}"
 
-if __name__ == 'main':
+@app.route('/sunrise-sunset')
+def sunrise_sunset():
+    url = 'https://api.sunrise-sunset.org/json'
+    lat = 50.08333
+    lng = 14.46667
+    return requests.get(url=url, params={'lat':lat, 'lng':lng}).json()
+
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT',5000))

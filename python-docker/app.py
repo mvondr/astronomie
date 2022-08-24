140502b342b1cc32612d0a11b732fc567e500715
Martin Vondráček <12727830+mvondr@users.noreply.github.com>
2022-08-24 09:11:50 +0200

    Update app.py
    Needs PORT env var

--- python-docker/app.py
+++ python-docker/app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+
+if __name__ == 'main'
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT'))

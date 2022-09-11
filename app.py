46e53d3f7da4bc37fd2b4146838bb93e95628611
mvondr <mvondr@gmail.com>
2022-09-11 15:44:20 +0200

    Index page AND CORS for localhost enabled

--- app.py
+++ app.py
@@ -1,25 +1,67 @@
 import os
 import requests
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def status():
-    return "{\"service\": \"sunrise-sunset\"}"
+def index():
+    return '''
+<!DOCTYPE html>
+<html>
+<body>
+<div>
+<p id='currentPosition'></p>
+<p id='positionName'>geoLocation</p>
+</div>
+<script>
+    var x = document.getElementById('currentPosition');
+    x.innerHTML = 'geoCoordinates'
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        x.innerHTML = 'Geolocation is not supported by this browser.';
+      }
+    }
+
+    function showPosition(position) {
+      x.innerHTML = 'Latitude: ' + position.coords.latitude +
+      '<br>Longitude: ' + position.coords.longitude;
+
+      var positionName = document.getElementById('positionName');
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', '/nominatim?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude);
+      xhr.send();
+      xhr.onload = () => {positionName.innerHTML =  xhr.responseText;
+        }
+    }
+    getLocation();
+
+
+
+</script>
+</body>
+</html>
+    '''
 
 @app.route('/sunrise-sunset')
 def sunrise_sunset():
     url = 'https://api.sunrise-sunset.org/json'
-    lat = 50.08333
-    lng = 14.46667
-    return requests.get(url=url, params={'lat':lat, 'lng':lng}).json()
+    lat=request.args['lat']
+    lon=request.args['lon']
+    return requests.get(url=url, headers={'Access-Control-Allow-Origin':'http://localhost:4200'}, params={'lat':lat, 'lng':lon}).json()
 
 @app.route('/nominatim')
 def nominatim():
     url = 'https://nominatim.openstreetmap.org/reverse'
-    lat = 50.08333
-    lng = 14.46667
-    return requests.get(url=url, params={'format': 'jsonv2', 'lat':lat, 'lon':lng}).json()
+    lat=request.args['lat']
+    lon=request.args['lon']
+    return requests.get(url=url, params={'format': 'jsonv2', 'lat':lat, 'lon':lon}).content
+@app.route('/geo-coordinates', methods=['GET'])
+def geoCoordinates():
+    lat=request.args['lat']
+    lon=request.args['lon']
+    return '{' + ''lat': {}, 'lon': {}'.format(lat, lon) + '}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT',5000))
